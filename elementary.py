d1b7fcd14022a163a99be7f2350699d8f91d820c
Grady Lynch <grlynch@chapman.edu>
2018-09-27 18:03:09 +0000

    Classwork upload

--- elementary.py
+++ elementary.py
@@ -29,7 +29,26 @@ class chargedparticle(particle):
     charge = 0.0
 
     def __init__(self, x,y,z)
-
+    
+class electron(chargedparticle):
+    """Electron is a subclass of chargedparticle. All instances of electron have a charge of -1.60217662e-19 coulombs and a mass of 9.10938356e-31 kilograms."""
+    
+    
+    def __init__(self, x,y,z):
+        self.charge = -1.60217662*10**(-19)
+        self.position = (x,y,z)
+        self.mass = 9.10938356*10**(-31)
+        self.momentum = (0.0,0.0,0.0)
+    
+class proton(chargedparticle):
+    """Proton is a subclass of chargedparticle. All instances of proton have a charge of 1.60217662e-19 coulombs and a mass of 1.6726219e-27 kilograms."""
+    
+    def __init__(self, x,y,z):
+        self.charge = 1.60217662*10**(-19)
+        self.position = (x,y,z)
+        self.mass = 1.6726219*10**(-27)
+        self.momentum = (0.0,0.0,0.0)
+    
 def main(argv):
     pass
 
