3674f75c8d9347227358c34fcd3d58d7d1f3853f
Conner Carnahan <ConnerCarnahan@gmail.com>
2018-09-27 17:38:56 +0000

    hey

--- elementary.py
+++ elementary.py
@@ -13,17 +13,24 @@ class particle(object):
     position = (0.0,0.0,0.0)
     momentum = (0.0,0.0,0.0)
     def __init__(self, x, y, z):
-        """Inits the class, arg1 is self (does something that has to do with self reference), arg2: x position float, arg3: y position float, arg4: z positionfloat"""
+        """Inits the class, arg1: x position float, arg2: y position float, arg3: z positionfloat"""
         self.position = (x,y,z)
         self.mass = 1.0
         self.momentum = (0.0,0.0,0.0)
 
     def impulse(self, px,py,pz):
-        """changes the momentum by """
+        """changes the momentum by an increment arg1: px, arg2: py, arg3: pz"""
         self.momentum = self.momentum + (px,py,pz)
 
     def move(self, dt):
-        self.position = self.position + dt*self.momentum
+        self.position = (self.position[0] + dt*self.momentum[0],self.position[1]+dt*self.momentum[1],self.position[2]+dt*self.momentum[2])
+
+class chargedparticle(particle):
+    charge = 0.0
+
+    def __init__(self, x,y,z)
 
 def main(argv):
+    pass
+
 
